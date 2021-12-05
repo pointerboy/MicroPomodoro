ed3de01d4123432bf1a11fbc396f25a82b8b9f24
pointerboy <lukatrbovic@outlook.com>
2021-12-05 18:38:20 +0100

    Removed unused imports on components

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View, ImageBackground} from 'react-native';
+import {StyleSheet, View, ImageBackground} from 'react-native';
 import Header from './components/Header';
 import PomodoroTimer from './components/PomodoroTimer';
 
